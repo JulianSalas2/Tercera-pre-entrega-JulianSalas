79b5af8c26ab4c7ebdfad5195ab514ee1a4651d1
JulianSalas2 <salasjulian544@gmail.com>
2024-11-23 20:12:18 -0500

    Se agregaron a models.py los modelos estudiante,profesor y entregable

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,3 +5,21 @@ from django.db import models
 class Curso(models.Model):
     Nombre = models.CharField(max_length= 30)
     Camada = models.IntegerField()
+    
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)   
+
+    fecha_de_entrega = models.DateField()
+    entregado = models.BooleanField()       
+
